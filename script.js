c20e2322a9710e2d5507dfea6fed583d44d8146d
Vily361 <v.pearl.dev@gmail.com>
2025-01-12 13:45:17 -0500

    Rework most of the game

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ rawCopper = 0
 rawCopperGained = 1
 rawCopperRequiredClicks = 5
 rawCopperRemainingClicks = 5
+rawCopperGainedUpgradeCost = 2
+timesRawCopperGainUpgraded = 0
 
 function RawCopperClicked(){
     rawCopperRemainingClicks--
@@ -11,4 +13,17 @@ function RawCopperClicked(){
         document.getElementById("rawCopperOwned").innerText = rawCopper
     }
     document.getElementById("rawCopperRemainingClicks").innerText = rawCopperRemainingClicks
+}
+
+function UpgradeRawCopperGained(){
+    if (rawCopper >= rawCopperGainedUpgradeCost){
+        rawCopper -= rawCopperGainedUpgradeCost
+        rawCopperGained += 1
+        rawCopperGainedUpgradeCost += 2
+        timesRawCopperGainUpgraded += 1
+        document.getElementById("rawCopperOwned").innerText = rawCopper
+        document.getElementById("rawCopperGained").innerText = rawCopperGained
+        document.getElementById("rawCopperGainUpgradeCost").innerText = rawCopperGainedUpgradeCost
+        document.getElementById("timesRawCopperGainUpgraded").innerText = timesRawCopperGainUpgraded
+    }
 }
